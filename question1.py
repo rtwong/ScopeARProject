afd226c33aab5f4bb9b4f0d778a3b5f60473f74f
Randy Wong <rtwong@ualberta.ca>
2016-10-04 19:25:49 -0700

    Raise exceptions in Q1, and 1a instead of just prints

--- question1.py
+++ question1.py
@@ -1,4 +1,7 @@
 def print_tree(height):
+	"""
+	Answer to question 1, prints a christmas tree of stars with a given height.
+	"""
 	# check if input is an int, a float, or garbage data
 	# use int, ignore float and garbage data, careful not to use a float as an int, easy to do in python
 	try:
@@ -7,23 +10,20 @@ def print_tree(height):
 		if float_check == int_check:
 			height = int_check
 	except:
-		print("Invalid, input is not an integer.")
-		return
+		raise ValueError("Invalid, input is not an integer.")
 
 
 	# check if we can even draw a tree
 	if height <= 0:
-		print("Invalid input, height is 0 or negative.")
-		return
-
-	if height == 1:
-		print("What a sad tree.")
+		raise ValueError("Invalid input, height is 0 or negative.")
 
 	for current_height in range(height):
 		# print leading spaces
 		print(" " * (height - current_height - 1), end="")
 		# print stars
 		print("*" * (current_height*2 + 1))
+
+	return 1
 
 
 user_input = input("Input a height, positive integers only: ")
--- question1a.py
+++ question1a.py
@@ -1,4 +1,7 @@
 def print_x(height):
+	"""
+	Answer to question 1a, prints an X using stars with a given height, (height >= 3, and height is odd) 
+	"""
 	# check if input is an int, a float, or garbage data
 	# use int, ignore float and garbage data, careful not to use a float as an int, easy to do in python
 	try:
@@ -7,14 +10,14 @@ def print_x(height):
 		if float_check == int_check:
 			height = int_check
 	except:
-		print("Invalid, input is not an integer.")
-		return
+		raise ValueError("Invalid, input is not an integer.")
+
 
 
 	# check if it is tall enough and odd, so we can draw an X
 	if height <= 2 or height%2 == 0:
-		print("Invalid input, height is 2 or negative, or an even number.")
-		return
+		raise ValueError("Invalid input, height is 2 or negative, or an even number.")
+
 
 	# create a list to hold spaces and stars, and move stars according to their height
 	# one moves up an index every loop, one moves back every loop. 
@@ -23,6 +26,8 @@ def print_x(height):
 		line[i] = "*"
 		line[height - 1 - i] = "*"
 		print("".join(line))
+
+	return 1
 
 
 user_input = input("Input a height, positive, odd integers only: ")
