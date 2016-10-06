dfb18ac6515b4e9a863debcf41c42860d7fc5d67
Randy Wong <rtwong@ualberta.ca>
2016-10-06 13:07:00 -0600

    FPS-like game made

---
+++ Scope_FPS_Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+
+    public static GameManager instance = null;
+
+    public int count;
+    private Text countText;
+
+
+    void Awake()
+    {
+        //Check if instance already exists
+        if (instance == null)
+
+            //if not, set instance to this
+            instance = this;
+
+        //If instance already exists and it's not this:
+        else if (instance != this)
+
+            //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+            Destroy(gameObject);
+
+        //Sets this to not be destroyed when reloading scene
+        DontDestroyOnLoad(gameObject);
+
+    }
+
+
+
+    // Use this for initialization
+    void Start () {
+        count = 0;
+        countText = GameObject.Find("Canvas/Score").GetComponent<UnityEngine.UI.Text>();
+        setCountText();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        setCountText();
+	}
+
+    void setCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+
+    }
+}
---
+++ Scope_FPS_Project/Assets/Scripts/ProjectileController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ProjectileController : MonoBehaviour {
+
+    public AudioSource hit_sound;
+
+	// Use this for initialization
+	void Start () {
+        hit_sound = GameObject.Find("Hit_sound").GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Target"))
+        {
+            GameManager.instance.count++;
+            hit_sound.Play();
+            Destroy(this.gameObject);
+        }
+    }
+
+
+}
